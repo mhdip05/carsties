b5c67197b8f948fede88b5880d85b8317cbbec12
MH Dip <mhdip05@outlook.com>
2024-06-04 22:30:19 -0600

    Gateway-Service

---
+++ src/AuctionService/Consumer/BidPlacedConsumer.cs
@@ -0,0 +1,30 @@
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumer
+{
+    public class BidPlacedConsumer : IConsumer<BidPlaced>
+    {
+        private readonly AuctionDbContext _dbContext;
+
+        public BidPlacedConsumer(AuctionDbContext context)
+        {
+            _dbContext = context;
+        }
+        public async Task Consume(ConsumeContext<BidPlaced> context)
+        {
+            Console.WriteLine("--> Consuming bid placed");
+
+            var auction = await _dbContext.Auctions.FindAsync(context.Message.AuctionId);
+
+            if(auction.CurrentHighBid == null 
+                || context.Message.BidStatus.Contains("Accepted") 
+                && context.Message.Amount > auction.CurrentHighBid)
+            {
+                auction.CurrentHighBid = context.Message.Amount;
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
